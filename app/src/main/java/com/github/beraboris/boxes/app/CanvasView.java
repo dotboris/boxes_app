e65728889e1ce7739fead78e5705389d35534c9b
Boris Bera <bboris@rsoft.ca>
2014-09-16 19:04:27 -0400

    Basic small drawing area in a big canvas
    The thing is done by hand and the path needs to be clipped to not go over the no draw zone

--- app/src/main/java/com/github/beraboris/boxes/app/CanvasView.java
+++ app/src/main/java/com/github/beraboris/boxes/app/CanvasView.java
@@ -17,15 +17,24 @@ public class CanvasView extends View {
     private Bitmap bitmap;
     private Canvas canvas;
 
+    private float canvasWidth;
+    private float canvasHeight;
+
     private OnTouchListener touchListener = new OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent event) {
+            RectF canvasBox = new RectF(canvasX(), canvasY(), canvasX() + canvasWidth, canvasY() + canvasHeight);
+
+            if (!canvasBox.contains(event.getX(), event.getY())) {
+                return false;
+            }
+
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    path.moveTo(event.getX(), event.getY());
+                    path.moveTo(event.getX() - canvasX(), event.getY() - canvasY());
                     break;
                 case MotionEvent.ACTION_MOVE:
-                    path.lineTo(event.getX(), event.getY());
+                    path.lineTo(event.getX() - canvasX(), event.getY() - canvasY());
                     break;
                 case MotionEvent.ACTION_UP:
                     canvas.drawPath(path, paint);
@@ -56,38 +65,59 @@ public class CanvasView extends View {
     }
 
     private void init(Context context, AttributeSet attrs, int style) {
+        canvasWidth = 300;
+        canvasHeight = 300;
+        int brushColor = Color.BLACK;
+        float brushWidth = 20;
+
+        if (attrs != null) {
+            TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CanvasView, style, 0);
+
+            try {
+                brushWidth = a.getDimension(R.styleable.CanvasView_brushWidth, brushWidth);
+                brushColor = a.getColor(R.styleable.CanvasView_brushColor, brushColor);
+
+                canvasHeight = a.getDimension(R.styleable.CanvasView_canvasHeight, canvasHeight);
+                canvasWidth = a.getDimension(R.styleable.CanvasView_canvasWidth, canvasWidth);
+            } finally {
+                a.recycle();
+            }
+        }
+
+        bitmap = Bitmap.createBitmap(
+                (int) Math.ceil(canvasWidth),
+                (int) Math.ceil(canvasHeight),
+                Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        canvas.drawColor(Color.WHITE);
+
         path = new Path();
         paint = new Paint(Paint.DITHER_FLAG);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeCap(Paint.Cap.ROUND);
         paint.setStrokeJoin(Paint.Join.ROUND);
-
-        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CanvasView, style, 0);
-
-        try {
-            paint.setColor(a.getColor(R.styleable.CanvasView_brushColor, Color.BLACK));
-            paint.setStrokeWidth(a.getDimension(R.styleable.CanvasView_brushWidth, 20));
-        } finally {
-            a.recycle();
-        }
+        paint.setColor(brushColor);
+        paint.setStrokeWidth(brushWidth);
 
         this.setOnTouchListener(touchListener);
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        canvas = new Canvas(bitmap);
-
-        super.onSizeChanged(w, h, oldw, oldh);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.translate(canvasX(), canvasY());
+
         canvas.drawBitmap(this.bitmap, 0, 0, paint);
         canvas.drawPath(path, paint);
+    }
+
+    private float canvasY() {
+        return (getHeight() / 2) - canvasHeight / 2;
+    }
+
+    private float canvasX() {
+        return (getWidth() / 2) - canvasWidth / 2;
     }
 
     public int getBrushColor() {
